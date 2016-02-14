5c2c20c8efa7f51885b9baa517e21ef4b003555c
tizz98 <dev.tizz98@gmail.com>
2016-02-14 17:02:56 -0500

    rotation wip...

--- app/src/main/java/org/zumh/android/draganddraw/Box.java
+++ app/src/main/java/org/zumh/android/draganddraw/Box.java
@@ -7,15 +7,22 @@ import android.os.Parcelable;
 public class Box implements Parcelable {
     private PointF mOrigin;
     private PointF mCurrent;
+    private float mRotation;  // in degrees
 
     public Box(PointF origin) {
+        this(origin, 0);
+    }
+
+    public Box(PointF origin, float rotation) {
         mOrigin = origin;
         mCurrent = origin;
+        mRotation = rotation;
     }
 
     public Box(Parcel source) {
         mOrigin.readFromParcel(source);
         mCurrent.readFromParcel(source);
+        mRotation = source.readFloat();
     }
 
     public PointF getOrigin() {
@@ -31,6 +38,14 @@ public class Box implements Parcelable {
         mCurrent = current;
     }
 
+    public float getRotation() {
+        return mRotation;
+    }
+
+    public void setRotation(float rotation) {
+        mRotation = rotation;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -40,6 +55,7 @@ public class Box implements Parcelable {
     public void writeToParcel(Parcel dest, int flags) {
         mOrigin.writeToParcel(dest, flags);
         mCurrent.writeToParcel(dest, flags);
+        dest.writeFloat(mRotation);
     }
 
     public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
--- app/src/main/java/org/zumh/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/org/zumh/android/draganddraw/BoxDrawingView.java
@@ -23,6 +23,9 @@ public class BoxDrawingView extends View {
     private Paint mBoxPaint;
     private Paint mBackgroundPaint;
 
+    private int mFirstPointerId = 0;
+    private PointF mModPtrDown;
+
     public BoxDrawingView(Context context) {
         this(context, null);
     }
@@ -50,25 +53,38 @@ public class BoxDrawingView extends View {
             float top = Math.min(box.getOrigin().y, box.getCurrent().y);
             float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
 
+            float px = right - (float) Math.floor((right - left) / 2);
+            float py = top - (float) Math.floor((top - bottom) / 2);
+
+            canvas.save();
+            canvas.rotate(box.getRotation(), px, py);
             canvas.drawRect(left, top, right, bottom, mBoxPaint);
+            canvas.restore();
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        PointF current = new PointF(event.getX(), event.getY());
+        int actionIdx = event.getActionIndex();
+        int ptrId = event.getPointerId(actionIdx);
+
+        PointF current = new PointF(event.getX(actionIdx), event.getY(actionIdx));
         String action = "";
 
-        switch (event.getAction()) {
+        switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
                 // reset drawing state
                 mCurrentBox = new Box(current);
                 mBoxen.add(mCurrentBox);
                 break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                action = "ACTION_POINTER_DOWN";
+                mModPtrDown = current;
+                break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
-                if (mCurrentBox != null) {
+                if (mCurrentBox != null && ptrId == mFirstPointerId) {
                     mCurrentBox.setCurrent(current);
                     invalidate();
                 }
@@ -77,13 +93,24 @@ public class BoxDrawingView extends View {
                 action = "ACTION_UP";
                 mCurrentBox = null;
                 break;
+            case MotionEvent.ACTION_POINTER_UP:
+                action = "ACTION_POINTER_UP";
+                if (ptrId == mFirstPointerId) {
+                    mCurrentBox = null;
+                } else {
+                    // angle it!
+                    float angle = (float) Math.toDegrees(Math.atan2(mModPtrDown.y - current.y, mModPtrDown.x - current.x));
+                    mCurrentBox.setRotation(angle);
+                    invalidate();
+                }
+                break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
                 mCurrentBox = null;
                 break;
         }
 
-        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+        Log.i(TAG, action + " w/ ptrid=" + ptrId + ", actionIdx=" + actionIdx + " at x=" + current.x + ", y=" + current.y);
         return true;
     }
 
